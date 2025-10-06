bc5117e42ebe082fede3ba7bb9c234f0d78190dd
Muhammad Ridho Fahru Rozy_F1D022076 <158574736+RidhoFahruRozy@users.noreply.github.com>
2025-10-06 22:31:19 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,20 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== SISTEM PEMBAYARAN ===");
 
-        // Buat daftar barang (otomatis)
         Product[] products = {
             new Product("Indomie Goreng", 3500, 3),
             new Product("Aqua Botol", 5000, 2),
             new Product("Kopi Kapal Api", 2500, 1)
         };
 
-        // Hitung total
         double total = 0;
         for (Product p : products) {
             total += p.getSubtotal();
         }
 
-        // Tampilkan daftar belanja
         System.out.println("\n=== DAFTAR BELANJA ===");
         for (Product p : products) {
             p.showProduct();
@@ -23,10 +20,8 @@ public class Main {
         System.out.println("----------------------------");
         System.out.printf("Total Belanja: Rp %.2f\n", total);
 
-        // Pilih metode pembayaran (contoh otomatis pakai E-Wallet)
         Payment payment = new EWalletPayment("TXN001", total, "DANA-0987");
 
-        // Proses pembayaran
         System.out.println("\n=== STRUK PEMBAYARAN ===");
         payment.processPayment();
 
